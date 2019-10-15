7b23cad9bfe4a40204026f16a3257f9fcff630ad
Saghir Bashir <saghir@ilustat.com>
2019-10-15 14:40:15 +0100

    Updated to pinp files, added tidyverse hex sticker, automated creation of README image.

--- _make.R
+++ _make.R
@@ -1,6 +1,8 @@
 ## Run all files to prepare "Getting Started in R" (Tidyverse Edition) Guide
 
 # Setup
+library(pdftools)
+library(magick)
 library(here)
 here()
 
@@ -9,7 +11,7 @@ rmarkdown::render("Getting-Started-in-R.Rmd")
 
 # Create zip files to share with participants
 # First empty the share folder and recreate the directory structure.
-unlink(here("Share/"), recursive = TRUE)
+unlink(here("Share/"), recursive = TRUE, force = TRUE)
 dir.create(here("Share"))
 dir.create(here("Share", "Getting-Started-in-R"))
 
@@ -26,3 +28,17 @@ rstudioapi::initializeProject(path = here("Share", "Getting-Started-in-R"))
 setwd(here("Share"))
 zip(here("Share", "Getting-Started-in-R.zip"), "./", extras = "-FS")
 setwd(here())
+
+# Create an image for the README.md file
+tempPNGs <- paste0("images/temp-", 1:2, ".png")
+pdf_convert(here("Getting-Started-in-R.pdf"), "png", pages = 1:2,
+            filenames = tempPNGs)
+
+img1 <- image_read(tempPNGs[1])
+img2 <- image_read(tempPNGs[2])
+
+image_append(c(image_border(img1, geometry = "3x3"),
+               image_border(img2, geometry = "3x3"))) %>%
+  image_write(.,path=here("images", "GSiR-tv-guide.png"), format="png")
+
+file.remove(tempPNGs)
