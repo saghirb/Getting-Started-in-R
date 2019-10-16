0736b7f43d39bbd811ec7eb89e5e6169aa589037
Saghir Bashir <saghir@ilustat.com>
2019-10-16 17:10:21 +0100

    Using temporary files in the _make.R

--- _make.R
+++ _make.R
@@ -30,7 +30,7 @@ zip(here("Share", "Getting-Started-in-R.zip"), "./", extras = "-FS")
 setwd(here())
 
 # Create an image for the README.md file
-tempPNGs <- paste0("images/temp-", 1:2, ".png")
+tempPNGs <- rep(tempfile(), 2)
 pdf_convert(here("Getting-Started-in-R.pdf"), "png", pages = 1:2,
             filenames = tempPNGs)
 
@@ -41,15 +41,11 @@ image_append(c(image_border(img1, geometry = "3x3"),
                image_border(img2, geometry = "3x3"))) %>%
   image_write(.,path=here("images", "GSiR-tv-guide.png"), format="png")
 
-file.remove(tempPNGs)
-
 # Image for GitHub settings
-ghTemp <- "images/ghTemp.png"
+ghTemp <- tempfile()
 test <- pdf_convert(here("Getting-Started-in-R.pdf"), "png", pages = 1, dpi = 155,
                     filenames = ghTemp)
 
   image_read(ghTemp) %>%
   image_crop(geometry_area(1280, 640, 0, 0), repage = FALSE) %>%
   image_write(., path=here("images", "GSiR-GitHub.png"), format="png")
-
-  file.remove(ghTemp)
